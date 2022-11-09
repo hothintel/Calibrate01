90c405e7a2ac0e3e54d2e5e6de1f44ad7c297cde
travishoth <travis@hothintel.com>
2022-11-09 17:38:20 -0600

    Initial commit

---
+++ Assets/Scripts/CalibrationManager.cs
@@ -0,0 +1,276 @@
+﻿//CalibrationManager
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+using TMPro;
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+using Microsoft.MixedReality.Toolkit.Input;
+using System.Text;
+
+#if ENABLE_WINMD_SUPPORT
+using Windows.Perception.Spatial;
+using HL2UnityPlugin;
+#endif
+
+public class CalibrationManager : MonoBehaviour, IMixedRealitySpeechHandler
+{
+#if ENABLE_WINMD_SUPPORT
+    HL2ResearchMode researchMode;
+#endif
+
+    bool enablePointCloud = true;
+
+    public TextMeshPro MyInfo;
+
+    private bool isUpdating = false;
+
+    private GameObject _origin;
+    private float _baseLineWidth = 0.001f;
+    private float _baseOriginLen = 0.1f;
+    private float _baseGradLen = 0.0025f;
+    public Color pointColor = Color.white;
+    private PointCloudRenderer _pointCloudRenderer;
+    public GameObject PointCloudRendererGo;
+
+    private ConcurrentQueue<string> ErrorQueue = new ConcurrentQueue<string>();
+    private ConcurrentQueue<List<Vector3>> SensorQueue = new ConcurrentQueue<List<Vector3>>();
+    private ConcurrentQueue<Vector3> SensorCoordQueue = new ConcurrentQueue<Vector3>();
+    private List<Vector3> CurrentSensorList = new List<Vector3>();
+    private Vector3 CurrentClosePoint = Vector3.zero;
+
+    private float[] currentBuffer = new float[] { };
+    private Vector3 _curCamPosition = Vector3.zero;
+    private Vector3 _curForward = Vector3.zero;
+    private float _calibrate = 0f;
+    private bool _renderPointCloud = true;
+    private bool _isLocked = false;
+
+#if ENABLE_WINMD_SUPPORT
+    Windows.Perception.Spatial.SpatialCoordinateSystem unityWorldOrigin;
+#endif
+
+    void Start()
+    {
+        MyInfo.text = "Starting...\r\n";
+        CoreServices.DiagnosticsSystem.ShowProfiler = false;
+        CoreServices.InputSystem.RegisterHandler<IMixedRealitySpeechHandler>(this);
+
+        if (PointCloudRendererGo != null)
+        {
+            _pointCloudRenderer = PointCloudRendererGo.GetComponent<PointCloudRenderer>();
+        }
+
+        //_curForward = CameraCache.Main.transform.forward;
+        // create origin axis
+        Material whiteMat = new Material(Shader.Find("Standard"));
+        whiteMat.SetColor("_Color", Color.white);
+        _origin = Helpers.CreateAxis(whiteMat, _baseLineWidth, _baseOriginLen, _baseGradLen);
+        _origin.transform.position = new Vector3(0, 0, 0);
+        _origin.transform.rotation = Quaternion.identity;
+
+        InitResearchMode();
+    }
+
+    private void InitResearchMode()
+    {
+#if ENABLE_WINMD_SUPPORT
+        researchMode = new HL2ResearchMode();
+        researchMode.InitializeLongDepthSensor();
+        researchMode.InitializeSpatialCamerasFront();
+
+        try
+        {
+            IntPtr WorldOriginPtr = UnityEngine.XR.WSA.WorldManager.GetNativeISpatialCoordinateSystemPtr();
+            unityWorldOrigin = Marshal.GetObjectForIUnknown(WorldOriginPtr) as Windows.Perception.Spatial.SpatialCoordinateSystem;
+
+            researchMode.SetReferenceCoordinateSystem(unityWorldOrigin);
+        }
+        catch(Exception myEx)
+        {
+            MyInfo.text += $"myEx={myEx.Message}\r\n";
+            unityWorldOrigin=null;
+        }
+
+        researchMode.SetPointCloudDepthOffset(0);
+        researchMode.StartLongDepthSensorLoop(enablePointCloud);
+        researchMode.StartSpatialCamerasFrontLoop();
+#endif    
+
+    }
+
+    private int maxFromCenter = 20;
+
+    void Update()
+    {
+        if (!isUpdating && !_isLocked)
+        {
+#if ENABLE_WINMD_SUPPORT
+            if (!researchMode.LongThrowPointCloudUpdated()) return;
+#endif
+            isUpdating = true;
+            if (SensorQueue.Count > 0)
+            {
+                List<Vector3> dequeuedPoints = new List<Vector3>();
+                if (SensorQueue.TryDequeue(out dequeuedPoints))
+                {
+                    CurrentSensorList = dequeuedPoints;
+                }
+
+                Vector3 closePt = Vector3.zero;
+                if (SensorCoordQueue.TryDequeue(out closePt))
+                    CurrentClosePoint = closePt;
+            }
+
+            currentBuffer = new float[] { };
+#if ENABLE_WINMD_SUPPORT
+            currentBuffer = researchMode.GetLongThrowPointCloudBuffer();
+#endif
+            _curCamPosition = CameraCache.Main.transform.position;
+            _curForward = CameraCache.Main.transform.forward;
+
+            Thread worker = new Thread(new ThreadStart(UpdateSensorPoints));
+            worker.Start();
+        }
+
+        if (_renderPointCloud)
+            _pointCloudRenderer.Render(CurrentSensorList.ToArray(), pointColor, CurrentClosePoint);
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        if (!focus) StopSensorsEvent();
+    }
+
+    public void StopSensorsEvent()
+    {
+#if ENABLE_WINMD_SUPPORT
+        researchMode.StopAllSensorDevice();
+#endif
+    }
+
+    private void UpdateSensorPoints()
+    {
+        isUpdating = true;
+
+        float closest = float.MaxValue;
+
+        List<Vector3> tempPoints = new List<Vector3>();
+        List<Vector3> sensorPoints = new List<Vector3>();
+        Vector3 closestPoint = Vector3.zero;
+        Vector3 deltaError = _curForward * _calibrate;
+        try
+        {
+            if (currentBuffer.Length == 0)
+                return;
+
+            int pointCloudLength = currentBuffer.Length / 3;
+            for (int i = 0; i < pointCloudLength; i++)
+            {
+                var x = currentBuffer[3 * i];
+                var y = currentBuffer[3 * i + 1];
+                var z = currentBuffer[3 * i + 2];
+                var curPt = new Vector3(x, y, z) + deltaError;
+
+                var dist = Vector3.Distance(_curCamPosition, curPt);
+                if (dist < 0.75f)
+                {
+                    tempPoints.Add(curPt);
+                    if (dist < closest)
+                    {
+                        closest = dist;
+                        closestPoint = curPt;
+                    }
+                }
+            }
+
+            for (int j = 0; j < tempPoints.Count; j++)
+            {
+                var dist = Vector3.Distance(tempPoints[j], closestPoint);
+                if (dist < 0.5f)
+                    sensorPoints.Add(tempPoints[j]);
+            }
+        }
+        catch (Exception exception)
+        {
+            string exMsg = "";
+            if (exception.Message.Contains("Out of Range"))
+                exMsg = "Out of Range";
+            else
+                exMsg = exception.Message;
+            string errMsg = $"Error: ex={exMsg}";
+            ErrorQueue.Enqueue(errMsg);
+        }
+        finally
+        {
+            if (sensorPoints.Count > 0)
+            {
+                SensorQueue.Enqueue(sensorPoints);
+                SensorCoordQueue.Enqueue(closestPoint);
+            }
+
+            isUpdating = false;
+        }
+
+    }
+
+    public static Vector3 CenterOfVectors(List<Vector3> vectors)
+    {
+        Vector3 sum = Vector3.zero;
+        if (vectors == null || vectors.Count == 0)
+            return sum;
+
+        foreach (Vector3 vec in vectors)
+            sum += vec;
+
+        return sum / vectors.Count;
+    }
+
+    public static Vector3 GetSmoothedPoint(Vector3 newPoint, ref List<Vector3> myPoints, int maxQueueLength)
+    {
+        myPoints.Add(newPoint);
+        if (myPoints.Count >= maxQueueLength)
+        {
+            myPoints.RemoveAt(0);
+        }
+
+        return CenterOfVectors(myPoints);
+    }
+
+    public void OnSpeechKeywordRecognized(SpeechEventData eventData)
+    {
+        //MyInfo.text += $"word={eventData.Command.Keyword.ToLower()}\r\n";
+
+        switch (eventData.Command.Keyword.ToLower())
+        {
+            case "lock scan":
+                _isLocked = true;
+                break;
+
+            case "start scan":
+                _isLocked = false;
+                break;
+
+            case "forward":
+                _calibrate -= 0.001f;
+                MyInfo.text += $"_calibrate={_calibrate}\r\n";
+                break;
+
+            case "backward":
+                _calibrate += 0.001f;
+                MyInfo.text += $"_calibrate={_calibrate}\r\n";
+                break;
+
+            default:
+                break;
+        }
+
+    }
+} // end of class
---
+++ Assets/Scripts/ElemRenderer.cs
@@ -0,0 +1,269 @@
+﻿using UnityEngine;
+using System.Linq;
+using System;
+using Microsoft.MixedReality.Toolkit.Utilities;
+
+public class ElemRenderer : MonoBehaviour
+{
+    Mesh mesh;
+
+
+    private void Awake()
+    {
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void UpdateMesh(Vector3[] arrVertices, int nPointsToRender, int nPointsRendered, Color pointColor, Vector3 closestPoint)
+    {
+        int nPoints;
+
+        if (arrVertices == null)
+        {
+            nPoints = 0;
+        }
+        else
+        {
+            nPoints = Math.Min(nPointsToRender, arrVertices.Length - nPointsRendered);
+        }
+        nPoints = Math.Min(nPoints, 65535);
+
+        Vector3[] points = arrVertices.Skip(nPointsRendered).Take(nPoints).ToArray();
+        int[] indices = new int[nPoints];
+        Color[] colors = new Color[nPoints];
+
+        for (int i = 0; i < nPoints; i++)
+        {
+            indices[i] = i;
+            //colors[i] = SetDepthColorClosest(points[i], closestPoint);
+            //colors[i] = SetColorTotalPoints(points[i], nPoints);
+
+            if (nPoints > 5000)
+            {
+                colors[i] = SetDepthColorClosestGreen(points[i], closestPoint);
+            }
+            else if (nPoints > 3000)
+            {
+                colors[i] = SetDepthColorClosestYellow(points[i], closestPoint);
+            }
+            else
+            {
+                colors[i] = SetDepthColorClosestRed(points[i], closestPoint);
+            }
+
+
+        }
+
+        //"Align with Patient's Face\nWhen Face is Aligned\nSay \"Lock Face\" or \"Cancel\"";
+
+        if (mesh != null)
+            Destroy(mesh);
+        mesh = new Mesh();
+        mesh.vertices = points;
+        mesh.colors = colors;
+        mesh.SetIndices(indices, MeshTopology.Points, 0);
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+    private Color SetColorTotalPoints(Vector3 point, int totalPoints)
+    {
+        //color = SetColor(255, 0, 0); //--- Red
+        //color = SetColor(255, 165, 0); //--- Orange
+        //color = SetColor(255, 255, 0); //--- Yellow
+        //color = SetColor(0, 128, 0); //--- Green
+        //color = SetColor(0, 0, 255); //--- Blue
+        //color = SetColor(75, 0, 130); //--- Indego
+        //color = SetColor(0, 0, 0, 0); //--- Black
+
+        Color color = SetColor(0, 0, 0);
+
+        if (totalPoints > 5000)
+        {
+            color = SetColor(0, 128, 0); //--- Green
+        }
+        else if (totalPoints > 3000)
+        {
+            color = SetColor(255, 255, 0); //--- Yellow
+        }
+        else
+        {
+            color = SetColor(255, 0, 0); //--- Red
+        }
+
+        return color;
+    }
+
+    private Color SetDepthColorClosest(Vector3 point, Vector3 closestPoint)
+    {
+        var camPos = CameraCache.Main.transform.position;
+
+        Color color = SetColor(255, 0, 0); //--- Red
+
+        try
+        {
+            var distToCam = Vector3.Distance(camPos, point);
+            var distClosestPoint = Vector3.Distance(camPos, closestPoint);
+            var dist = distToCam - distClosestPoint;
+
+            if (dist > 0.15f)
+            {
+                color = SetColor(0, 0, 0, 0); //--- Black
+            }
+            else if (dist > 0.125f)
+            {
+                color = SetColor(75, 0, 130); //--- Indego
+            }
+            else if (dist > 0.1f)
+            {
+                color = SetColor(0, 0, 255); //--- Blue
+            }
+            else if (dist > 0.075f)
+            {
+                color = SetColor(0, 128, 0); //--- Green
+            }
+            else if (dist > 0.05f)
+            {
+                color = SetColor(255, 255, 0); //--- Yellow
+            }
+            else if (dist > 0.025f)
+            {
+                color = SetColor(255, 165, 0); //--- Orange
+            }
+        }
+        catch (Exception)
+        {
+
+        }
+
+        return color;
+    }
+
+    private Color SetDepthColorClosestGreen(Vector3 point, Vector3 closestPoint)
+    {
+        var camPos = CameraCache.Main.transform.position;
+
+        //008F00(0, 143, 0)
+        //00B800(0, 184, 0)
+        //47FF47(71, 255, 71)
+        //ADFFAD(173, 255, 173)
+
+        Color color = SetColor(0, 143, 0); //--- 1 Green X
+
+        try
+        {
+            var distToCam = Vector3.Distance(camPos, point);
+            var distClosestPoint = Vector3.Distance(camPos, closestPoint);
+            var dist = distToCam - distClosestPoint;
+
+            if (dist > 0.15f)
+            {
+                color = SetColor(0, 0, 0, 0); //--- 5 Black
+            }
+            else if (dist > 0.1f)
+            {
+                color = SetColor(173, 255, 173); //--- 4 X
+            }
+            else if (dist > 0.05f)
+            {
+                color = SetColor(71, 255, 71); //--- 3 X 
+            }
+            else if (dist > 0.025f)
+            {
+                color = SetColor(0, 184, 0); //--- 2 X
+            }
+        }
+        catch (Exception)
+        {
+
+        }
+
+        return color;
+    }
+
+    private Color SetDepthColorClosestYellow(Vector3 point, Vector3 closestPoint)
+    {
+        var camPos = CameraCache.Main.transform.position;
+
+        Color color = SetColor(184, 184, 0); //--- 1 Yellow X
+
+        try
+        {
+            var distToCam = Vector3.Distance(camPos, point);
+            var distClosestPoint = Vector3.Distance(camPos, closestPoint);
+            var dist = distToCam - distClosestPoint;
+
+            if (dist > 0.15f)
+            {
+                color = SetColor(0, 0, 0, 0); //--- 5 Black
+            }
+            else if (dist > 0.1f)
+            {
+                color = SetColor(255, 255, 214); //--- 4 X
+            }
+            else if (dist > 0.05f)
+            {
+                color = SetColor(255, 255, 112); //--- 3 X 
+            }
+            else if (dist > 0.025f)
+            {
+                color = SetColor(255, 255, 10); //--- 2 X
+            }
+        }
+        catch (Exception)
+        {
+
+        }
+
+        return color;
+    }
+
+    private Color SetDepthColorClosestRed(Vector3 point, Vector3 closestPoint)
+    {
+        var camPos = CameraCache.Main.transform.position;
+
+        Color color = SetColor(204, 0, 0); //--- 1 Red X
+
+        try
+        {
+            var distToCam = Vector3.Distance(camPos, point);
+            var distClosestPoint = Vector3.Distance(camPos, closestPoint);
+            var dist = distToCam - distClosestPoint;
+
+            if (dist > 0.15f)
+            {
+                color = SetColor(0, 0, 0, 0); //--- 5 Black
+            }
+            else if (dist > 0.1f)
+            {
+                color = SetColor(255, 194, 194); //--- 4 X
+            }
+            else if (dist > 0.05f)
+            {
+                color = SetColor(255, 112, 112); //--- 3 X 
+            }
+            else if (dist > 0.025f)
+            {
+                color = SetColor(255, 31, 31); //--- 2 X
+            }
+        }
+        catch (Exception)
+        {
+
+        }
+
+        return color;
+    }
+
+    private static Color SetColor(byte red, byte green, byte blue, byte opacity = 255)
+    {
+        return new Color32(red, green, blue, opacity);
+    }
+}
